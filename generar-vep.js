ec3be94983f0ad38b2027e5dacb75008a8f26d63
Javier Martin Gonzalez <javiermartingonzalez@gmail.com>
2022-06-23 11:35:31 -0300

    WIP con vep generator

--- generar-vep.js
+++ generar-vep.js
@@ -31,21 +31,29 @@ async function main() {
   await page.click('input[name="F1:btnIngresar"]');
   await navigationPromise;
   //await page.waitForTimeout(2000);
-  await page.click('text=Monotributo');
+  await page.click('text=Adhesión y/o empadronamiento al monotributo, modificación de datos e ingreso de claves de confirmación');
 
   await page.waitForTimeout(3000);
 
   const pages = await context.pages();
   const monotributoPage = pages[1];
 
+  const navigationPromiseMonotributo = monotributoPage.waitForNavigation({
+    waitUntil: 'domcontentloaded'
+  });
+
+
   //await navigationPromiseM;
 
   // Pagina
   await monotributoPage.click('a[id="aBtn1"]');
   await monotributoPage.waitForTimeout(1000);
+  await navigationPromiseMonotributo;
   //await navigationPromiseM;
   // Pagina
-  await monotributoPage.click('img[title="Pago mis cuentas"]');
+  //await monotributoPage.click('img[title="Pago mis cuentas"]');
+  // aca quede..
+  await monotributoPage.locator('img[title="Pago mis cuentas"]').click();
   await monotributoPage.waitForTimeout(1000);
   //await navigationPromiseM;
   // Pagina
