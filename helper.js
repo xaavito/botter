3451526fc11fb6b7e170582cc79af222345b7ed4
Javier Martin Gonzalez <javiermartingonzalez@gmail.com>
2022-08-03 08:37:45 -0300

    WIP

--- helper.js
+++ helper.js
@@ -16,6 +16,17 @@ async function login(page) {
     await page.click('input[name="F1:btnIngresar"]');
 }
 
+async function comprobantesEnLinea(page) {
+    await page.goto('https://auth.afip.gob.ar/contribuyente_/login.xhtml');
+    await page.waitForSelector('input[name="F1:username"]');
+    await page.fill('input[name="F1:username"]', process.env.USER_CUIL);
+    await page.click('input[name="F1:btnSiguiente"]');
+    await page.waitForSelector('input[name="F1:password"]', { visible: true });
+    await page.fill('input[name="F1:password"]', process.env.USER_PASS);
+    await page.click('input[name="F1:btnIngresar"]');
+}
+
+
 module.exports = {
     rounder,
     login
