216061841b3d7a040aad2433851364a96ea1103d
Javier Martin Gonzalez <javiermartingonzalez@gmail.com>
2026-02-23 21:55:34 -0300

    wip

--- actions/excel.js
+++ actions/excel.js
@@ -1,13 +1,13 @@
 const { generar } = require('./generar')
-const { listar } = require('./listar')
-const { listarOnline } = require('./listarOnline')
-const logger = require('./logger')
-const { writeToFile, readFromFile } = require('./helper.js')
-const { ExcelProcessor } = require('./excelProcessor')
+
+const { ExcelProcessor } = require('../helpers/excelProcessor')
 
 async function excel() {
   const excelProcessor = new ExcelProcessor()
-  resultados = await excelProcessor.ejecutar(generar)
+  const resultados = await excelProcessor.ejecutar(generar)
 
-  return resultados
+  return resultados;
 }
+
+
+module.exports = { excel }
